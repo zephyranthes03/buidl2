72c336c0363009c66d07746129e518a27cda9cba
YJ chong <zephyranthes03@gmail.com>
2024-02-25 14:47:23 -0700

    Camera test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,7 +68,7 @@ export default function Home() {
               </span>
             </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Appended link Templates
+              Control camera example
             </p>
           </a>
       </div>

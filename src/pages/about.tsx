a6e0bc4daaa2598f46d3f86a48e2d14211edf63a
YJ chong <zephyranthes03@gmail.com>
2024-02-25 11:20:18 -0700

    version1

---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function About() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
+      Hello world
+      </main>
+  );
+}

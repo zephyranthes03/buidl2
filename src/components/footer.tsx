293f9d7ab1f8563849cd44936f0333145d2f49b0
YJ <zephyranthes03@gmail.com>
2024-03-05 16:03:02 -0700

    Updating Templete

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const Footer: React.FC = () => {
   return(
-    <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <footer>
+    <div className="fixed bottom-0 left-0 flex h-12 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none font-mono">
       <a
         className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
         href="https://github.com/zephyranthes03/buidl2"
@@ -15,5 +16,6 @@ export const Footer: React.FC = () => {
         Support by FTS project team.
       </a>
   </div>
+  </footer>
   );
 };

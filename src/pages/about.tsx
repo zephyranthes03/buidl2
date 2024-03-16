dbba5a72d889c9352bc005b31d059e853f4a7a74
YJ chong <zephyranthes03@gmail.com>
2024-03-15 21:07:05 -0600

    Fix logo position on about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,10 +9,10 @@ const inter = Inter({ subsets: ["latin"] });
 export default function About() {
   return (    
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-8 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-left justify-between p-12 ${inter.className}`}
     >
       <Header />
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+      <div className="relative flex justify-center items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
           src="/Group 1394.png"

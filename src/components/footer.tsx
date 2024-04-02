bacd273858ec3cc9e706e0c8ff3d09723fe8d577
YJ chong <zephyranthes03@gmail.com>
2024-04-02 15:17:12 -0600

    Update header/footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,7 +8,7 @@ export const Footer: React.FC = () => {
     <footer>
     <div className="fixed bottom-0 left-0 flex h-12 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none font-mono">
       <a
-        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+        className="pointer-events-none flex place-items-center gap-2 p-2 lg:pointer-events-auto lg:p-0"
         href="https://github.com/zephyranthes03/buidl2"
         target="_blank"
         rel="noopener noreferrer"
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,9 +6,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const Header: React.FC = () => {
   return(
-    <header className="max-w-2xl w-full">
-    <div className="z-10 max-w-2xl items-start justify-between font-mono text-base lg:flex">
+    <header>
+    <div className="fixed top-8 left-0 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none font-mono">
+
     {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"> */}
+    <a href='/'>
     <Image
       className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
       src="/Group 1395.png"
@@ -17,6 +19,7 @@ export const Header: React.FC = () => {
       height={26}
       priority
     />
+    </a>
       {/* <code className="font-mono font-bold">src/pages/index.tsx</code>
     </p> */}
     {/* <div className="fixed bottom-0 left-3 flex h-12 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
